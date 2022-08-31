557998ea8ccc4a1b3605718db4154798291c9716
Matheus Telles <business.mwtelles@gmail.com>
2022-08-31 00:31:29 -0300

    Estilizando e tratando botão de login

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login__button');
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
@@ -11,9 +12,22 @@ const handleFocusOut = ({ target }) => {
         const span = target.previousElementSibling;
         span.classList.remove('span-active');
     }
+}
+
+const handleChange = () => {
+    const [username, password] = inputs;
+
+    if (username.value && password.value.length >= 8) {
+
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', '');
+    }
     
 }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
 
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+
+inputs.forEach((input) => input.addEventListener('input', handleChange));
